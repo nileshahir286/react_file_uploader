28d1ab8f098fa3de2067c1f6a8efb169860f1802
Nilesh N Ahir <nileshahir286@gmail.com>
2021-10-08 12:46:39 +0530

    second commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.post('/upload', (req, res) => {
   if (req.files === null) {
     return res.status(400).json({ msg: 'No file uploaded' });
   }
-//update
+
   const file = req.files.file;
 
   file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
